8c7166a125714cea1ee12085c4f7ca5532a2161a
citron03 <cchan0617@gmail.com>
2022-05-09 14:41:46 +0900

    feat: 채점 api 연결(임시)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Home, Mypage, MissionList, CodeEdit, RegisterMission, MissionDetail, Feedback } from './pages';
 import Notification from "./components/Notification";
 import { useLogin } from "./utils/login";
 
+const queryClient = new QueryClient();
+
 function App() {
   useLogin();
   return (
     <BrowserRouter>
-        <Header/>
-        <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route path="/mypage" element={<Mypage/>} />
-              <Route path="/missions" element={<MissionList/>} />
-              <Route path="/mission/:id" element={<MissionDetail/>} />
-              <Route path="/feedback/:id" element={<Feedback/>} />
-              <Route path="/edit" element={<CodeEdit/>} />
-              <Route path="/register" element={<RegisterMission/>} />
-        </Routes>
-        <Footer/>
-        <Notification/>
+        <QueryClientProvider client={queryClient}>
+          <Header/>
+          <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route path="/mypage" element={<Mypage/>} />
+                <Route path="/missions" element={<MissionList/>} />
+                <Route path="/mission/:id" element={<MissionDetail/>} />
+                <Route path="/feedback/:id" element={<Feedback/>} />
+                <Route path="/edit" element={<CodeEdit/>} />
+                <Route path="/register" element={<RegisterMission/>} />
+          </Routes>
+          <Footer/>
+          <Notification/>
+        </QueryClientProvider>
     </BrowserRouter>
   );
 }
