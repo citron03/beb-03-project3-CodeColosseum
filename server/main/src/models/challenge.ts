4b4b712a0dbb7fb4692242c6d63792ed744b851b
odoldotol <odoldotol@gmail.com>
2022-05-10 18:39:11 +0900

    update grading to return passedCases

--- server/main/src/models/challenge.ts
+++ server/main/src/models/challenge.ts
@@ -5,7 +5,7 @@ const ChallengeSchema = new Schema(
     user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // 제출자
     mission: { type: Schema.Types.ObjectId, ref: "Mission", required: true }, // 문제
     answerCode: { type: String, required: true }, // 코드
-    passedCase: { type: [Boolean], required: true }, // [true, false, true, ...] 테스트케이스 정답여부
+    passedCases: { type: [Boolean], required: true }, // [true, false, true, ...] 테스트케이스 정답여부
     // time: { type: Number, required: true }, // 제출시간(초)
     // kind: { type: String, required: true }, // 제출 종목 (콜로세움, 연습 등)
   },
