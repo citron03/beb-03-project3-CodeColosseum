a123dcb7e67ed92c357ae48a2e7462ce8ebed79b
odoldotol <odoldotol@gmail.com>
2022-05-10 18:40:14 +0900

    update grading to return passedCases

--- server/grading/src/routes/grading.ts
+++ server/grading/src/routes/grading.ts
@@ -4,15 +4,15 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   console.log("채점 요청 받음");
-  const { missionId, code, testCase } = req.body;
+  const { missionId, code, testCases } = req.body;
 
   try {
-    const result = await getSolution(missionId, code, testCase);
+    const result = await getSolution(missionId, code, testCases);
 
-    if (result === 0) {
+    if (result.falseCount === 0) {
       res.status(200).send({ message: "complete" });
     } else {
-      res.status(200).send({ message: "fail", failCount: result });
+      res.status(200).send({ message: "fail", failCount: result.falseCount, passedCases: result.passedCases });
     }
   } catch (err) {
     console.log(err);
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
 const getSolution = async (
   mission: string,
   code: string,
-  testCase: { input: any; output: any }[]
+  testCases: { input: any; output: any }[]
 ) => {
   const fs = await import("fs");
   const timeStamp = +new Date();
@@ -31,17 +31,22 @@ const getSolution = async (
   fs.writeFileSync(fileName, `${code} exports.solution = solution;`);
   const getJsFile = await require(`../../${fileName}`);
 
+  let passedCases : Boolean[] = []
   let falseCount = 0;
-  for (let test of testCase) {
+  for (let testCase of testCases) {
     if (
-      JSON.stringify(getJsFile.solution(...test.input)) !==
-      JSON.stringify(test.output)
+      JSON.stringify(getJsFile.solution(...testCase.input)) !==
+      JSON.stringify(testCase.output)
     ) {
       falseCount += 1;
+      passedCases.push(false);
+    }
+    else {
+      passedCases.push(true);
     }
   }
   fs.unlinkSync(fileName);
-  return falseCount;
+  return {falseCount, passedCases};
 };
 
 export default router;
