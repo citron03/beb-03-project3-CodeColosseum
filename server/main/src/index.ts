ff3a05dcd608c63ffa06b5302571291cadbd91bf
odoldotol <odoldotol@gmail.com>
2022-05-11 19:39:26 +0900

    fix: npm run dev

--- server/main/src/index.ts
+++ server/main/src/index.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: ["http://localhost:3000"],
     credentials: true,
-    methods: ["OPTIONS", "GET", "POST", "PATCH", "PUT"],
+    methods: ["OPTIONS", "GET", "POST", "PATCH", "PUT", "DELETE"],
   })
 );
 
