d52a98adf57f5c3d85c7ef75f2568830c85db54f
citron03 <cchan0617@gmail.com>
2022-05-17 13:32:07 +0900

    fix: footer 단어 수정 (white -> light)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             </S.Div>
             <S.Div>
                 <S.P>이 페이지에는 네이버에서 제공한 나눔글꼴이 적용되어 있습니다.</S.P>
-                <S.Span onClick={() => dispatch(modeChange())}>{state ? "make white" : "make dark"}</S.Span>
+                <S.Span onClick={() => dispatch(modeChange())}>{state ? "make light" : "make dark"}</S.Span>
             </S.Div>
         </S.Footer>);
 }
