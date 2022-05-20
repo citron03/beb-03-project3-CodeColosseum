386d84e4b1cd78763f4453d4e8122f67e8ef3199
citron03 <cchan0617@gmail.com>
2022-05-20 11:13:29 +0900

    fix: 사용되지 않는 페이지 삭제, 에디터 최대 크기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import GlobalStyle from "./GlobalStyle";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Home, Mypage, MissionList, CodeEdit, RegisterMission, MissionDetail, Feedback, About } from './pages';
+import { Home, Mypage, MissionList, RegisterMission, MissionDetail, Feedback, About } from './pages';
 import { Loading, Notification, SignUp, DisappearingNotification } from "./components/Modals";
 import { useLogin } from "./utils/login";
 import { useSelector } from "react-redux";
@@ -25,7 +25,6 @@ function App() {
                 <Route path="/mission/colosseum/:id" element={<MissionDetail isColosseum={true}/>}/>
                 <Route path="/mission/:id" element={<MissionDetail isColosseum={false}/>}/>
                 <Route path="/feedback/:id" element={<Feedback/>}/>
-                <Route path="/edit" element={<CodeEdit/>}/>
                 <Route path="/register" element={<RegisterMission/>}/>
                 <Route path="/about" element={<About/>}/>
           </Routes>
