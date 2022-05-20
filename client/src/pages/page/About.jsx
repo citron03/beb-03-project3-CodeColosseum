d856f3d3bc35b4b9a70a57c0a9c57f3ca916e91f
citron03 <cchan0617@gmail.com>
2022-05-20 09:45:45 +0900

    fix: About 페이지 처음 입장시 화면 위치 고정

--- client/src/pages/page/About.jsx
+++ client/src/pages/page/About.jsx
@@ -1,9 +1,12 @@
 import S from "./About.styled";
 import { CoverImage, Introduce, Navigator } from "../../components/About";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const About = () => {
     const introduceRef = useRef();
+    useEffect(() => {
+        window.scrollTo({top: 123});
+    }, []);
     return (
         <S.About>
             <CoverImage introduceRef={introduceRef}/>
