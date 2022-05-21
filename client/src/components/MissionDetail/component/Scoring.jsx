5abbf42bcc196159f5b424d3d643a818d028f3a8
citron03 <cchan0617@gmail.com>
2022-05-21 22:12:10 +0900

    fix: 채점 후 메세지 수정

--- client/src/components/MissionDetail/component/Scoring.jsx
+++ client/src/components/MissionDetail/component/Scoring.jsx
@@ -12,28 +12,35 @@ const Scoring = ({grading, id}) => {
 
     useEffect(() => {
         console.log("채점 결과: ", grading.data); // {failCount: 0, passedCases: Array(5), reward: null, isClosed: true}
-        if(grading?.data?.reward){
-            // 내가 답을 맞춤
-            dispatch(showNotification(`정답입니다!\n${grading.data.reward}개의 토큰을 획득하였습니다.`));
-            // navigate(`/feedback/${id}`); 평가 페이지 이동
-            return;
-        }
-        if(grading?.data?.isClosed){
-            // 다른 사람이 정답 맞춤
-            dispatch(showNotification("아쉽네요!\n다른 사람이 먼저 정답을 맞췄습니다."));
-            // navigate("/");
-            return;
-        }
-        if(grading?.message){
-            if(grading?.message === "Grading Fail, Code Error"){
-                dispatch(showNotification("코드 에러!"));
+        if(grading.data){
+            if(grading?.data?.reward){
+                // 내가 답을 맞춤 (콜로세움 문제)
+                dispatch(showNotification(`정답입니다!\n${grading.data.reward}개의 토큰을 획득하였습니다.`));
+                // navigate(`/feedback/${id}`); 평가 페이지 이동
+                return;
             }
-            else if(grading?.data?.failCount > 0){
-                dispatch(showNotification("채점 완료!\n 하지만 아쉽게도 틀렸습니다."));
-                setMessage(`아쉽네요!`);
-            } else{
-                dispatch(showNotification("채점 완료!\n 모든 테스트를 통과하셨습니다!"));
-                setMessage("축하합니다!");
+            if(grading?.data?.isClosed){
+                // 다른 사람이 정답 맞춤 (콜로세움 문제)
+                dispatch(showNotification("아쉽네요!\n다른 사람이 먼저 정답을 맞췄습니다."));
+                // navigate("/");
+                return;
+            }
+            if(!grading?.data?.timeSafe){
+                // 타임 오버
+                dispatch(showNotification("아쉽네요!\n주어진 시간을 모두 소모하였습니다."));
+                return;            
+            }
+            if(grading?.data){
+                if(grading?.data?.codeError){
+                    dispatch(showNotification("코드 에러!"));
+                }
+                else if(grading?.data?.isPassed){
+                    dispatch(showNotification("채점 완료!\n 모든 테스트를 통과하셨습니다!\n평가를 남겨주세요 ★"));
+                    setMessage("축하합니다!");
+                } else{
+                    dispatch(showNotification("채점 완료!\n 하지만 아쉽게도 틀렸습니다."));
+                    setMessage(`아쉽네요!`);
+                }
             }
         }
     }, [grading, dispatch, navigate])
