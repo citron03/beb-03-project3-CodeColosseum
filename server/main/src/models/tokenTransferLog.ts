ba88cb5aec759441ce77ff7c939fb1b0ce4a9c5d
odoldotol <odoldotol@gmail.com>
2022-05-23 17:57:45 +0900

    fix: tokenRewardColosseum

--- server/main/src/models/tokenTransferLog.ts
+++ server/main/src/models/tokenTransferLog.ts
@@ -10,7 +10,7 @@ const schema = new Schema(
     code: { type: Number, required: true }, // <TokenTransferLogCode> 0:?? 1: 콜로세움도전 2: 위너보상 3: 출제자보상 4: nft보상수금 5: 교역소 6?: 상품구매? 7: 토큰구매
     for: { type: Object, required: true }, // <TokenTransferLogFor> {collection, id}
     token: { type: String, required: true }, // 토큰 Symbol
-    amount: { type: Number, required: true },
+    amount: { type: String, required: true },
     paymentAt: { type: Date }, // 대략적인 전송 시각
   },
   { timestamps: true }
