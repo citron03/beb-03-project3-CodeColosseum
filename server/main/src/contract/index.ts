1f7ffd938123e06712f2ee1c757320380d31a815
odoldotol <odoldotol@gmail.com>
2022-05-26 07:49:49 +0900

    feat: checkMineOwner

--- server/main/src/contract/index.ts
+++ server/main/src/contract/index.ts
@@ -4,5 +4,18 @@ import createTokenTransferLog from './createTokenTransferLog';
 import tokenRewardColosseum from './tokenRewardColosseum';
 import createMiningMineralLog from './createMiningMineralLog';
 import mineralTradingExcution from './mineralTradingTxExcution';
+import uploadMissionMetadata from './uploadMissionMetadata';
+import mineOwnershipNftMint from './mineOwnershipNftMint';
+import checkMineOwner from './checkMineOwner';
 
-export = { feeDelegatedTxExcution, tokenPaymentResDataColosseum, createTokenTransferLog, tokenRewardColosseum, createMiningMineralLog, mineralTradingExcution };
+export = {
+    feeDelegatedTxExcution,
+    tokenPaymentResDataColosseum,
+    createTokenTransferLog,
+    tokenRewardColosseum,
+    createMiningMineralLog,
+    mineralTradingExcution,
+    uploadMissionMetadata,
+    mineOwnershipNftMint,
+    checkMineOwner
+};
