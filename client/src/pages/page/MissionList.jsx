e10e1d70bc64d0806867bfc6c0d193113275cdc1
citron03 <cchan0617@gmail.com>
2022-05-25 11:53:19 +0900

    fix: 미션 로그인 체크 훅이 과도하게 일어나는 에러 수정

--- client/src/pages/page/MissionList.jsx
+++ client/src/pages/page/MissionList.jsx
@@ -2,9 +2,12 @@ import S from "./MissionList.styled";
 import { Missions, ListTitle } from "./../../components/MissionList";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useCheckLogin } from "./../../utils/login";
 
 const MissionList = ({isColosseum}) => {
     const query = isColosseum ? 1 : 4;
+
+    useCheckLogin(); // 로그인 확인
 
     const { data } = useQuery([`/mission/list`, query], async () => {
         return axios.get(`/mission/list?category=${query}`)
