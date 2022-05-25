6fb1764df20aa8a976917b33c9f7b189fe0bc615
LeeJooYong <daramzycoder@gmail.com>
2022-05-25 15:43:23 +0900

    fix : 채점 오류 수정

--- server/main/src/controller/mission/challengePractice.ts
+++ server/main/src/controller/mission/challengePractice.ts
@@ -10,14 +10,24 @@ const post = async (req: any, res: any) => {
   try {
     const userInfo = await models.User.findOne({ account });
     const missionInfo = await models.Mission.findOne({ _id: missionId });
+    const isTest = reqType === 1 ? true : false;
     const testCases =
       reqType === 1
         ? missionInfo.testCases.filter((testCase: any) => {
             return testCase.isExample === true;
           })
         : missionInfo.testCases;
+
+    // console.log(testCases);
     const gradingResult = await gradingMission(testCases, code);
 
+    // console.log(gradingResult);
+    const isPassed =
+      gradingResult.data === undefined
+        ? false
+        : gradingResult.data.failCount === 0
+        ? true
+        : false;
     try {
       let challenge;
       if (reqType === 2) {
@@ -26,7 +36,7 @@ const post = async (req: any, res: any) => {
           mission: missionId,
           kind: 2,
           answerCode: code,
-          isPassed: gradingResult.data.failCount === 0 ? true : false,
+          isPassed,
           PassedCasesRate: `${
             testCases.length - gradingResult.data.failCount
           } / ${testCases.length}`,
@@ -36,15 +46,17 @@ const post = async (req: any, res: any) => {
       }
 
       // 풀이에 성공한 경우 미네랄 지급
-      if (gradingResult.data.failCount === 0) {
+      const challengeInfo = await models.Challenge.findOne(challenge);
+      if (isPassed) {
         console.log("미네랄 지급 시작");
         // TODO
         // challenger에게 +mineral
         // mineral log 기록
         // 중복 지급 방지
-        const challengeInfo = await models.Challenge.findOne(challenge);
         await contract.createMiningMineralLog(challengeInfo);
-        await updateUserMineralBalance(userInfo.id);
+        console.log("createMingMineralLog 완료");
+        await updateUserMineralBalance(userInfo._id);
+        console.log("updateUserMineral 완료");
 
         console.log("미네랄 지급 완료");
 
@@ -52,9 +64,15 @@ const post = async (req: any, res: any) => {
         await editMineOwnerRewardLog("reward", missionId);
       }
 
-      res
-        .status(200)
-        .send({ message: gradingResult.message, data: gradingResult.data });
+      console.log(challengeInfo);
+      res.status(200).send({
+        message: gradingResult.message,
+        data: {
+          ...gradingResult.data,
+          isPassed: challengeInfo.isPassed,
+          isTest,
+        },
+      });
     } catch (err) {
       console.log(err);
       res.status(400).send({ message: "DB upload Error" });
