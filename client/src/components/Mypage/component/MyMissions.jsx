3259b05277c12c264eae24bbb1b2bb21f6aa9b7f
citron03 <cchan0617@gmail.com>
2022-05-26 17:48:23 +0900

    fix: 빈 배열 오류 수정 (내가 출제한 문제들)

--- client/src/components/Mypage/component/MyMissions.jsx
+++ client/src/components/Mypage/component/MyMissions.jsx
@@ -6,11 +6,13 @@ import MissionSelector from "./MissionSelector";
 const MyMissions = ({userCreatedMissions}) => {
     // 1: 콜로세움,  2: 풀림(풀렸지만 NFT화는 안됨), 3:NFT로 만들어짐(NFT화가 되었지만 공개는 안함), 4:연습문제로 공개됨
     const [selector, setSelector] = useState({"1": true, "2": true, "4": true});
-    const [missionsArr, setMissionsArr] = useState([...userCreatedMissions]);
+    const [missionsArr, setMissionsArr] = useState([]);
 
     useEffect(() => {
-        const newArr = userCreatedMissions.filter(el => selector[el.state]);
-        setMissionsArr(newArr);
+        if(userCreatedMissions?.length > 0) {            
+            const newArr = userCreatedMissions.filter(el => selector[el.state]);
+            setMissionsArr(newArr);
+        }
     }, [selector, userCreatedMissions])
 
     return (
